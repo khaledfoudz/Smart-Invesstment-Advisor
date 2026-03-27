4dd887c38fc58929fa8ed1153434850343a7c915
[Mazenfarouk7] <kfouda2005@gmail.com>
2026-03-27 17:23:26 +0200

    changes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,17 +3,15 @@ import HeroSection from "@/components/HeroSection";
 import FeaturesSection from "@/components/FeaturesSection";
 import DashboardSection from "@/components/DashboardSection";
 import Footer from "@/components/Footer";
-import PriceWidget from "@/components/PriceWidget";
+
 const Index = () => {
   return (
     <div className="relative">
       <Navbar />
       <HeroSection />
       <FeaturesSection />
-      <PriceWidget />
       <DashboardSection />
       <Footer />
-      
     </div>
   );
 };
